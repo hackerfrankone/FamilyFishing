399e64f9df36cb54d02414c2ed459e25121f72f0
hackerfrankone <hackerfrankone@gmail.com>
2025-09-02 18:44:11 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,12 +7,11 @@ self.addEventListener('install', (event) => {
         '/submit.html',
         '/winners.html',
         '/images/background.png',
-        '/images/icon-192x192.png',
-        '/images/icon-512x512.png',
+        '/images/app.png',
+        '/images/app-512x512.png',
         '/manifest.json',
         '/PDFs/rules.pdf',
-        '/PDFs/PRIVACY-POLICY.pdf',
-        '/pages/tackle.html'
+        '/PDFs/PRIVACY-POLICY.pdf'
       ]);
     })
   );
@@ -22,7 +21,6 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request).catch(() => {
-        // Fallback to cached index.html for failed network requests
         return caches.match('/index.html');
       });
     })
